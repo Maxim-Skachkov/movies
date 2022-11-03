6c3c71a2fcdb6b6528917a9e985cec690366c47c
Max <logist.sklad.marvel@gmail.com>
2022-11-03 11:50:24 +0300

    tiny fix

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from .models import *
@@ -10,5 +10,11 @@ class MoviesView(View):
     def get(self, request):
         movies = Movie.objects.all()
         context = {'movie_list': movies}
-        return render(request, 'movies/movie_list.html', context)
+        return render(request, 'movies/movies.html', context)
 
+
+class MovieDetailView(View):
+    def get(self, request, slug):
+        movie = get_object_or_404(Movie, url=slug)
+        context = {'movie': movie}
+        return render(request, 'movies/movie_detail.html', context)
